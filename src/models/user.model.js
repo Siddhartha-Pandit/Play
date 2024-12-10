f1516fea8ac46cea1b9ca441ada1bbcb5f821b80
Siddhartha <siddharthapandit66@gmail.com>
2024-12-10 15:14:32 +0530

    Add refresh token api

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -58,7 +58,10 @@ userSchema.pre("save", async function (next){
     next()
 })
 userSchema.methods.isPasswordCorrect = async function(password){
-    await bcrypt.compare(password, this.password)
+   return(await bcrypt.compare(password, this.password))
+   
+  
+    
 }
 
 userSchema.methods.generateAccessToken=function(){
