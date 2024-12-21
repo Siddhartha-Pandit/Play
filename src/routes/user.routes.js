3d09835760b2925a81490800d2f658aea6ff3a3d
Siddhartha <siddharthapandit66@gmail.com>
2024-12-21 10:25:45 +0530

    assignment

---
+++ src/routes/user.routes.js
@@ -0,0 +1,34 @@
+import {Router} from "express"
+import { loginUser, logoutUser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js";
+import { verifyJwt } from "../middlewares/auth.Middleware.js";
+
+const router=Router()
+
+router.route("/register").post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser
+)
+
+router.route("/login").post(loginUser)
+
+//secured route
+router.route("/logout").post(verifyJwt,logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJwt,changeCurrentPassword)
+router.route("/current-user").get(verifyJwt,getCurrentUser)
+router.route("/update-account").patch(verifyJwt,updateAccountDetails)
+router.route("/avatar").patch(verifyJwt,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJwt,upload.single("coverImage"),updateUsercoverImage)
+router.route("/c/:username").get(verifyJwt,getUserChannelProfile)
+router.route("/history").get(verifyJwt,getWatchHistory)
+export default router
