854a898b66122832dc84824fcb72d917c8c6ffe6
Siddhartha <siddharthapandit66@gmail.com>
2024-12-21 09:23:52 +0530

    write the controller for watch history

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,11 +2,11 @@ import mongoose,{Schema} from "mongoose"
 
 const subscriptionSchema=new Schema({
     subscriber:{
-        type:Schema.Types.ObjectId //one who is subscribing
+        type:Schema.Types.ObjectId, //one who is subscribing
         ref:"User"
     },
     channel:{
-        type:Schema.Types.ObjectId //the one who is being subscribed
+        type:Schema.Types.ObjectId ,//the one who is being subscribed
         ref:'User'
     }
 },
